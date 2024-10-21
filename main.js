077cc3b86e3232df0edd452d1f90dc475c366a91
Roan Ali <alir13@usf.edu>
2024-10-21 19:48:20 -0400

    Calculate and update total price dynamically based on product and quantity

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 //Extracts data from HTML
 const productDropdown = document.getElementById('product-dropdown');
 const quantityInput = document.getElementById('quantity');
-const totalPrice = document.getElementById('total-price');
+const totalPriceElement = document.getElementById('total-price');
 
 const placeOrderButton = document.getElementById('place-order'); 
 const orderSummary = document.getElementById('order-summary');
 
+//Task 3:Calculate and update total price dynamically based on product and quantity
+//Creating a Function to Update total price
+
+function updateTotalPrice() {
+    const productPrice = parseFloat(productDropdown.value);
+    const quantity = parseInt(quantityInput.value);
+    const totalPrice = productPrice * quantity;
+    
+    totalPriceElement.textContent = totalPrice.toFixed(2);
+}
 //Adds event listeners to dropdown 
 productDropdown.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice);
